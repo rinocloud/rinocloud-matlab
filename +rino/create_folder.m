6aacb148ed6ae8d74983cd5f2c93fc385fc7cc22
jamie47 <james.patrick.lee.47@gmail.com>
2016-01-23 22:26:52 +0000

    create_folder added

---
+++ +rino/create_folder.m
@@ -0,0 +1,32 @@
+function [ response_struct ] = create_folder( folder_name )
+%UNTITLED10 Summary of this function goes here
+%   Detailed explanation goes here
+
+    checkfoldername(folder_name);
+
+    %Get APIToken
+    APIToken = rino.authentication;
+    
+    %create http headers
+    headers = [rino.http_createHeader('Authorization',APIToken), rino.http_createHeader('Content-Type','application/json')];
+
+    response = rino.urlread2(strcat(rino.api,'/files/create_folder/'),'POST',savejson('', struct('name',folder_name)), headers);
+    
+    try
+        response_struct = rino.loadjson(response);
+    catch
+        response_struct = response;
+    end
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Input verification functions
+    function TF = checkfoldername(x)
+        TF = false;
+        if ischar(x)
+            TF = true;
+        else
+            error('Specify the folder name as a string.');
+        end
+    end
+
+end
+
