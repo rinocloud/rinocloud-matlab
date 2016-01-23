f73c4d4ef1e114ea002b2defc61ad4240843f6ee
jamie47 <james.patrick.lee.47@gmail.com>
2016-01-23 17:06:01 +0000

    updata metadata function done

---
+++ +rino/update_metadata.m
@@ -0,0 +1,54 @@
+function [ response_struct ] = update_metadata(ID, metadata )
+%update_metadata - used to update the metadata of an object. You must
+%specify the object ID and the new metadata as a struct.
+%   Detailed explanation goes here
+
+    %Check inputs
+    checkID(ID);
+    checkmetadata(metadata);
+
+    %Get APIToken
+    APIToken = rino.authentication;
+    
+    %JSONify metadata_struct
+    metadatajson = rino.savejson('', rino.catstruct(metadata, struct('id', ID)), struct('Compact', 1));
+    
+    %Prepare http headers
+    headers = [rino.http_createHeader('Authorization',APIToken), rino.http_createHeader('Content-Type','application/json')];
+
+    %Make post request
+    response = rino.urlread2(strcat(rino.api,'/files/update_metadata/'),'POST',metadatajson, headers);
+    
+    try
+        response_struct = rino.loadjson(response);
+    catch
+        response_struct = response;
+    end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%Checking inputs
+    function TF = checkID(x)
+        TF = false;
+        if isstr(x) || isnumeric(x)
+            if isstr(x)
+               if length(str2num(x))<1
+                   error('File must be specified by its object ID - the object ID is a number, not a file name.')
+               end
+            end
+            TF = true;
+        else
+            error('File should be specified and the object ID of the parent folder. This can be specified as a string or a number.');
+        end
+    end
+
+    function TF = checkmetadata(x)
+        TF = false;
+        if isstruct(x)
+            TF = true;
+        else
+            error('The metadata should be passed to the update_metadata function as a structure array. See http://uk.mathworks.com/help/matlab/ref/struct.html for information of how to create structure arrays.');
+        end
+    end
+
+end
+
