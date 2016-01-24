066d2cd8e46ef58aa4fa88ac99e125d7eccb8189
jamie47 <james.patrick.lee.47@gmail.com>
2016-01-24 18:47:20 +0000

    tests added

--- +rino/authentication.m
+++ +rino/authentication.m
@@ -1,7 +1,7 @@
 function [ output_args ] = authentication( ~ )
 %authentication - returns the authentication token
 % 
-Token='df8f94649696abc16270369405a2f491b7c2cfd0';
+Token='a377055b6aecc41f00038c4cd48169b6b55b3d78';
 output_args=['Token ',Token];   
 
 end
